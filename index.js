db96cb35ab4cbdf2475b5d62683abdd1d23fa9ea
Amin Tai <amintai157@gmail.com>
2021-05-21 15:18:20 +0530

    qoutes added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const data = require('./data')
+var data = require('./data')
 function getQoute(){
     var random = Math.random()
     var res = Math.floor(random*100)
-    if(res > 10 ) 
-        res = res - 10
+    if(res > 56 ) res = res - 56
+    console.log(data[res])
     return data[res]
 }
 
